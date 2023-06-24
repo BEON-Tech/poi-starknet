3f7b65962075892e8535fd763ff7489997994d70
David Mazzitelli <davidma1984@gmail.com>
2023-06-24 03:15:59 -0300

    feat: removed hardcoded data from deployment script

--- scripts/deploy-contract.ts
+++ scripts/deploy-contract.ts
@@ -1,11 +1,14 @@
 import hardhat from "hardhat";
 import { starknet } from "hardhat";
+import { Config } from "./config";
 
 async function main() {
   const account = await getOZAccount();
   const contractFactory = await hardhat.starknet.getContractFactory("poi");
   await account.declare(contractFactory, { maxFee: 1e18 });
-  const contract = await account.deploy(contractFactory);
+  const contract = await account.deploy(contractFactory, {
+    owner: Config.publicKey,
+  });
   console.log("Deployed to:", contract.address);
 }
 
@@ -21,9 +24,7 @@ main()
  */
 export async function getOZAccount() {
   return await starknet.OpenZeppelinAccount.getAccountFromAddress(
-    // address from previous step
-    "0x26a9305844b65263360d6ea85aa1426a4e75e76a3247a876f8e68ef4c4b2067",
-    // private key from previous step
-    "0xd64583c27cc7443e2fc3ce912684bafecf8587abf08d054531d3d2c96d6bd63"
+    Config.publicKey,
+    Config.privateKey
   );
 }
